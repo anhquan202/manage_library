f42e5551c6769fcbca2b4296b12762d9f7951525
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2024-06-15 00:01:13 +0700

    Refactor UI

--- frontend/aside/aside.php
+++ frontend/aside/aside.php
@@ -2,41 +2,62 @@
   <h2 class="title">Dashboard</h2>
   <div id="menu-list">
     <div class="children"><a href="#dashboard">Trang chủ</a></div>
-    <div class="children">
+    <div class="children has-children">
       <div class="chidren-header">
-        <a href="#books">Quản lý Sách</a>
+        <span>Quản lý Sách</span>
         <i class="fa-solid fa-chevron-down"></i>
       </div>
       <ul class="children-books">
-        <li><a href=""></a>Tác giả</li>
-        <li><a href=""></a>Nhà xuất bản</li>
-        <li><a href=""></a>Sách</li>
+        <li><a href="authors.php">Tác giả</a></li>
+        <li><a href="publishers.php">Nhà xuất bản</a></li>
+        <li><a href="books.php">Sách</a></li>
       </ul>
     </div>
-    <div class="children"><a href="#members">Quản lý Thành viên</a></div>
-    <div class="children"><a href="#loans">Quản lý Mượn/Trả</a></div>
-    <div class="children"><a href="#reports">Báo cáo</a></div>
+    <div class="children">
+      <a href="./member/member.php">Quản lý Thành viên</a>
+    </div>
+    <div class="children">
+      <a href="loans.php">Quản lý Mượn/Trả</a>
+    </div>
+    <div class="children">
+      <a href="reports.php">Báo cáo</a>
+    </div>
   </div>
-  <!-- <h2>Tìm kiếm</h2>
-  <form action="#search" method="get">
-    <input type="text" name="query" placeholder="Tìm kiếm sách...">
-    <button type="submit">Tìm kiếm</button>
-  </form> -->
 </aside>
 
 <script>
-    // Lắng nghe sự kiện click trên phần tử cha
-    document.getElementById('menu-list').addEventListener('click', (event) => {
-      // Kiểm tra nếu phần tử được click có class 'children'
-      if (event.target.closest('.children')) {
-        const clickedItem = event.target.closest('.children');
-        
-        // Loại bỏ class 'abc' khỏi tất cả các phần tử
-        document.querySelectorAll('.children').forEach((item) => {
-          item.classList.remove('abc');
-        });
-        // Thêm class 'abc' vào phần tử được click
-        clickedItem.classList.add('abc');
-      }
-    });
-  </script>
+  document.getElementById('menu-list').addEventListener('click', (event) => {
+    if (event.target.closest('.children')) {
+      const clickedItem = event.target.closest('.children');
+
+      document.querySelectorAll('.children').forEach((item) => {
+        item.classList.remove('active');
+      });
+
+      clickedItem.classList.add('active');
+    }
+  });
+  const list_page = document.querySelectorAll('.children a');
+  list_page.forEach((item) => {
+    item.addEventListener('click', (event) => {
+      event.preventDefault();
+      const page = item.getAttribute('href');
+      fetch(page)
+        .then(res => {
+          if (!res.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return res.text();
+        })
+        .then(data => {
+          // main-content id in index.php
+          console.log(data);
+          document.getElementById('main-content').innerHTML = data;
+        })
+        .catch(error => {
+          console.error('There has been a problem with your fetch operation:', error);
+          document.getElementById('content').innerHTML = '<p>Đã xảy ra lỗi khi tải nội dung.</p>';
+        })
+    })
+  })
+</script>
