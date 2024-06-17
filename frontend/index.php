9f71d59c20f188beaf3d9d425866e9ae4fbadcf3
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2024-06-17 19:14:02 +0700

    Add index page

---
+++ frontend/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Trang chủ</title>
+  <?php require('head.php') ?>
+</head>
+
+<body>
+  <?php require('header/header.php') ?>
+  <div class="content">
+    <?php require('./aside/aside.php') ?>
+
+    <div id="main-content">
+      <!-- dynamic content loading -->
+
+    </div>
+  </div>
+</body>
+
+</html>
