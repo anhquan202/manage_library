85667adf9fe5c2e342d1a0e2f9b384973e9e2f90
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2024-07-22 10:48:38 +0700

    Remove js code

--- frontend/header/header.php
+++ frontend/header/header.php
@@ -62,16 +62,3 @@ if (isset($_COOKIE['jwt'])) {
     </div>
   </div>
 </div>
-<script>
-  const dropdown_button = document.querySelector('.dropdown-button');
-  const dropdown_content = document.querySelector('.dropdown-content');
-  dropdown_button.addEventListener('click', () => {
-    dropdown_content.classList.toggle('isOpen');
-    event.stopPropagation();
-  })
-  document.addEventListener('click', (event) => {
-    if (dropdown_content.classList.contains('isOpen') && !dropdown_content.contains(event.target) && !dropdown_button.contains(event.target)) {
-      dropdown_content.classList.remove('isOpen');
-    }
-  });
-</script>
